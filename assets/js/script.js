24438cb95a8ec992a8485c2064bcb03036df639b
Bryan Dumpit <dumpitbr@gmail.com>
2022-08-15 17:20:46 -0400

    fix search bugs and add functionality for recalling previous searches in the same session

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 //------------------DEFINE VARIABLES--------------------------------------:
 
-    //Hotels com API api through rapidapi.com
+    //Hotels com API through rapidapi.com
     //searches nearby hotels relative to the query(geoposition: latitude and longitude)
 const optionsHotel = {
 	method: 'GET',
@@ -38,12 +38,19 @@ var modalTitleEl = document.querySelector("#modal-title")
 
 var modalBodyEl = document.querySelector("#modal-body")
 
+var recallContainerEl = document.querySelector('#recall')
+
+var userInput=[]
+
+
 
 //-----------------DEFINE FUNCTIONS----------------------:
-//user input (origin, destination and time departure-default: current time) is passed through this function; data repackaged and sent to fetch function
+//this function is called after pressing Search button and validates value of each input
 var inputValidation = function (){
     //initial input validation
-    var modalKey ='';  
+    var modalKey =''; 
+    
+    userInput.length=0;//resets userInput array on each search so data from previous search is not carried over
 
     if (originInputEl.value===''){
         modalKey="invalid-origin";
@@ -58,72 +65,150 @@ var inputValidation = function (){
         modalKey="invalid-checkout";
         modalHandler(modalKey);
     } else {
-        convertDestinationGeoPos();
+        dateHandler();//calls dateHandler function
     }
 }
 
-var convertDestinationGeoPos = function(){
-    console.log('this will convert user input destination string to latitude and longitude')
-  
-    var destinationInput = destinationInputEl.value.trim();
-    var destinationGeoPosUrl= 'https://google-maps-geocoding.p.rapidapi.com/geocode/json?address=' + destinationInput + '&language=en'
-    console.log(destinationGeoPosUrl);
+//transforms date to YYYY-MM-DD format
+var dateHandler = function(){
     
     
-    fetch(destinationGeoPosUrl, optionsGeoPos)
+    var dateInput = []//empty array; push API response based on input into array
+    console.log(dateInput);
+
+    var checkIn = checkInEl.value;
+    
+    checkIn = moment(checkIn).format("YYYY-MM-DD");
+    var checkInIdentifier = {identifier: 'checkin'};
+    var checkInObj = {date: checkIn};
+    var checkInData = Object.assign(checkInObj,checkInIdentifier);
+    console.log(checkInData);
+    dateInput.push(checkInData);
+    
+    var checkOut = checkOutEl.value;
+    
+    checkOut = moment(checkOut).format("YYYY-MM-DD")
+    var checkOutIdentifier = {identifier: 'checkout'};
+    var checkOutObj = {date: checkOut};
+    var checkOutData = Object.assign(checkOutObj,checkOutIdentifier);
+    console.log(checkOutData);
+    dateInput.push(checkOutData);
+
+    originHandler(dateInput);//passes dates to originHandler; originHandler will add its data to the array
+}
+//converts origin string input into geoposition data
+var originHandler = function(input){
+    
+    console.log(input)
+    userInput.push(input);//pushed dateInput (parameter: input) into the global userInput array
+
+    //defines API url for fetch function
+    var originInput = originInputEl.value.trim();
+    var originGeoPosUrl= 'https://google-maps-geocoding.p.rapidapi.com/geocode/json?address=' + originInput + '&language=en'
+    console.log(originGeoPosUrl);
+    //fetches geolocation data for string input by user
+    fetch(originGeoPosUrl, optionsGeoPos)
         .then(function(response){
             if (response.ok){
                 response.json().then(function(data){
                     console.log(data);
-                    console.log(data.status)
-                    if(data.results.length===0){
-                        reset();
-                        var modalKey = "no-destination"
-                        modalHandler(modalKey);
+                    if (data.results.length!==0){
+                        var originIdentifier = {identifier:'origin'}//need to add identifier since order of data pushed into array is not definite (i.e. varying response times);
+                        var originData = Object.assign(data,originIdentifier)
+                        console.log(originData);
+                        userInput.push(originData);
+                        destinationHandler(userInput);//calls next input handler (destination handler); and sends the userInput package so next handler can add its data to the package.
                     } else {
-                        fetchHotelsFunction(data);
-                        console.log('else executed')
-                    }                    
+                        originInputEl.value ='';
+                        var modalKey = "no-origin";
+                        modalHandler(modalKey);
+                    }                   
                 })
             }
         })
         .catch(function(error){
             console.log(error);
             reset();
-            var modalKey = "destination-error"
+            var modalKey = "origin-error"
             modalHandler(modalKey);
         });
-};
 
+    console.log(userInput)
+}
+//converts destination string input into geoposition data
+var destinationHandler = function(userInput){
+    //API URL fo fetch
+    var destinationInput = destinationInputEl.value.trim();
+    var destinationGeoPosUrl= 'https://google-maps-geocoding.p.rapidapi.com/geocode/json?address=' + destinationInput + '&language=en'
+    console.log(destinationGeoPosUrl);
+    //fetches geolocation based on destination string input by user
+    fetch(destinationGeoPosUrl, optionsGeoPos)
+    .then(function(response){
+        if (response.ok){
+            response.json().then(function(data){
+                console.log(data);
+                console.log(data.status)
+                if(data.results.length===0){
+                    reset();
+                    var modalKey = "no-destination"
+                    modalHandler(modalKey);
+                } else {
+                    var destinationIdentifier = {identifier:'destination'};
+                    var destinationData = Object.assign(data,destinationIdentifier)
+                    console.log(destinationData);
+                    userInput.push(destinationData);
+                    console.log(userInput)
+                    fetchHotels(userInput);//by this point all the required data has been packaged into userInput (checkIn, checkOut, origin, destination); 
+                    //sends to subsequent function to unpack relevant data to be processed
+                }                    
+            })
+        }
+    })
+    .catch(function(error){
+        console.log(error);
+        reset();
+        var modalKey = "destination-error"
+        modalHandler(modalKey);
+    });
 
-
-//search nearby hotels by lat and lon
-var fetchHotelsFunction = function(destinationGeoPos){
-    console.log('this fetches list of nearby hotels by lat and lon')
-    console.log(destinationGeoPos)
+}
+ //fetches nearby hotels to destination identified by user   
+var fetchHotels = function (userInput) {
+      
+    console.log(userInput);
     
-    var checkIn = checkInEl.value;
-    
-    checkIn = moment(checkIn).format("YYYY-MM-DD")
+    var destinationInput = {};//empty object
+   
+    //retrieve destination from data package
+    for (var i = 0; i<userInput.length; i++){
+        if (userInput[i].identifier==='destination'){
+            destinationInput = userInput[i];//reassign data retrieved from data package
+            break;
+        }
+    }
+    console.log(destinationInput);
+
+    //retrieve checkin and checkout time from data package
+    var checkIn = userInput[0][0].date
     console.log(checkIn);
-    
-    var checkOut = checkOutEl.value;
-    
-    checkOut = moment(checkOut).format("YYYY-MM-DD")
+    var checkOut = userInput[0][1].date
     console.log(checkOut)
-    
 
-    var destinationLat = destinationGeoPos.results[0].geometry.location.lat;
-    var destinationLng = destinationGeoPos.results[0].geometry.location.lng;
-       
+
+    //define parameters to be used for apiUrl
+    var destinationLat = destinationInput.results[0].geometry.location.lat;
+    var destinationLng = destinationInput.results[0].geometry.location.lng;
+    
+    
+    //API URL for hotel fetch   
     var hotelApiUrl = 'https://hotels-com-provider.p.rapidapi.com/v1/hotels/nearby?latitude='+destinationLat+'&currency=USD&longitude='+destinationLng+'&checkout_date='+checkOut+'&sort_order=STAR_RATING_HIGHEST_FIRST&checkin_date='+checkIn+'&adults_number=1&locale=en_US&guest_rating_min=4&star_rating_ids=3%2C4%2C5&children_ages=4%2C0%2C15&page_number=1&price_min=10&accommodation_ids=20%2C8%2C15%2C5%2C1&theme_ids=14%2C27%2C25&price_max=500&amenity_ids=527%2C2063'
     fetch(hotelApiUrl, optionsHotel)
         .then(function(response){
             if (response.ok){
                 response.json().then(function(data){
-                    console.log(data);
                     if (data.searchResults.results.length!==0){
-                        hotelListHandler(data);
+                        console.log(data);
+                        hotelListHandler(data,userInput);//data from response is sent to handler function to add elements to DOM
                     } else {
                         reset();
                         var modalKey = "no-hotel"
@@ -139,13 +224,13 @@ var fetchHotelsFunction = function(destinationGeoPos){
             modalHandler(modalKey);
         })
 
-        //output data to create DOM elements and user can pick a hotel
-};
+}
 
 //creates interactive element (currently buttons) for nearby hotels identified
-var hotelListHandler = function(hotelList){
-    console.log(hotelList,"create Elements based on hotels listed");
-    hotelResults = hotelList.searchResults.results;
+var hotelListHandler = function(hotelList,userInput){
+    console.log(hotelList);
+    console.log(userInput);
+    hotelResults = hotelList.searchResults.results;//sets the hotels identified into global variable hotelResults
     console.log(hotelResults);
 
     hotelContainerEl.replaceChildren();
@@ -164,18 +249,19 @@ var hotelListHandler = function(hotelList){
 //once user picks a hotel,identifies which hotel from hotels object was clicked
 var selectedHotel = function(event){
     var hotel = event.target
+    console.log(userInput)//global array userInput was continuously updated in input handler functions above; didnt need to run this as an argument/parameter
     console.log(hotel);
     console.log(hotelResults);//data captured from hotelListHandler function
     console.log('this will fetch from directions api')
     console.log(hotel.textContent)
 
     var index ='';
-
+    //makes sure the subsequent tasks only happen if the click was on the button not the div container
     if (event.target.getAttribute("data-id")==='hotel-button'){
         for (var i = 0; i<hotelResults.length; i++) {
             if (hotelResults[i].name === hotel.textContent){
                 console.log(hotelResults[i].name);
-                index = i;
+                index = i;//assigns the i of the loop to var index
                 break
             } else if (hotelResults[i].name !== hotel.textContent){
                 continue
@@ -187,45 +273,13 @@ var selectedHotel = function(event){
         var hotelSelected = hotelResults[index];
     
         console.log(hotelSelected);
-    
-        convertOriginGeoPos(hotelSelected);
+        recallHandler(userInput,hotelSelected)//calls function to make buttons for search history
+        initTravelMap(userInput,hotelSelected)//calls function to update intial map from initMap()
+     
     }        
 }
 
-//takes origin input from user-filled form and selected hotel data from hotel list generated
-var convertOriginGeoPos = function(hotel){
-    console.log('this will convert user input origin string to latitude and longitude')
-    
-    console.log(hotel);
-
-    var originInput = originInputEl.value.trim();
-    var originGeoPosUrl= 'https://google-maps-geocoding.p.rapidapi.com/geocode/json?address=' + originInput + '&language=en'
-    console.log(originGeoPosUrl);
-    
-    fetch(originGeoPosUrl, optionsGeoPos)
-        .then(function(response){
-            if (response.ok){
-                response.json().then(function(originGeoPos){
-                    console.log(originGeoPos);
-                    if (originGeoPos.results.length!==0){
-                        initTravelMap(originGeoPos,hotel);
-                    } else {
-                        originInputEl.value ='';
-                        var modalKey = "no-origin";
-                        modalHandler(modalKey);
-                    }                   
-                })
-            }
-        })
-        .catch(function(error){
-            console.log(error);
-            reset();
-            var modalKey = "origin-error"
-            modalHandler(modalKey);
-        });
-};
-
-//displays initial map; lets user know that the website will generate a map visual
+//displays initial map; lets user know the website provides a map
 function initMap (){
     var mapOptions ={ 
         center:{lat:56.1304
@@ -235,37 +289,59 @@ function initMap (){
 }
 
 //generates zoomed map, draws route and displays direction steps
-var initTravelMap = function (origin,hotel){
+var initTravelMap = function (input,hotel){
+    
+    
+
+    console.log(input)
+    console.log(hotel)
+
+    var originInput ={}
+
+    //retrieve origin data from data package
+    for (var i=0; i<input.length; i++){
+        if (input[i].identifier==='origin'){
+            originInput=input[i];
+            break;
+        }
+    }
+
+    console.log(originInput);
+
+    var originLat = originInput.results[0].geometry.location.lat;
+    var originLng = originInput. results[0].geometry.location.lng;
     
     document.getElementById('map').replaceChildren();
 
     const directionsService = new google.maps.DirectionsService();
     const directionsRenderer = new google.maps.DirectionsRenderer();
     
-    var hotelLatLng=hotel;
-    console.log(hotelLatLng);
+    
 
     var mapOptions ={ 
-    center:{lat:origin.results[0].geometry.location.lat
-        ,lng:origin.results[0].geometry.location.lng},
+    center:{lat:originLat
+        ,lng:originLng},
         zoom:14}
     const map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
+    
+
     directionsRenderer.setMap(map);//renders map
-    document.getElementById('directionsPanel').replaceChildren();
-    directionsRenderer.setPanel(document.getElementById('directionsPanel'));//provides steps
-    calculateAndDisplayRoute (directionsService,directionsRenderer,origin, hotelLatLng);
-
+    
+    directionsRenderer.setPanel(document.getElementById('directionsPanel'));
+    calculateAndDisplayRoute (directionsService,directionsRenderer,originInput, hotel,userInput);
 }
+//function that adds route and directions
+var calculateAndDisplayRoute = function (directionsService,directionsRenderer,originInput,hotel,userInput) {
+    
+    document.getElementById('directionsPanel').replaceChildren();
 
-function calculateAndDisplayRoute (directionsService,directionsRenderer,origin,hotelLatLng) {
-
-    var hotelLat = hotelLatLng.coordinate.lat;
-    var hotelLng = hotelLatLng.coordinate.lon;
+    var hotelLat = hotel.coordinate.lat;
+    var hotelLng = hotel.coordinate.lon;
     console.log(hotelLat,hotelLng);
 
-    var originLat = origin.results[0].geometry.location.lat;
-    var originLng = origin.results[0].geometry.location.lng;
+    var originLat = originInput.results[0].geometry.location.lat;
+    var originLng = originInput. results[0].geometry.location.lng;
 
     console.log(originLat,originLng);
     
@@ -278,7 +354,7 @@ function calculateAndDisplayRoute (directionsService,directionsRenderer,origin,h
         travelMode: 'DRIVING',//default travel mode is driving
     })
     .then((response)=>{
-        directionsRenderer.setDirections(response);
+        directionsRenderer.setDirections(response);//adds directions
     })
     .catch(function(error){
         console.log(error);
@@ -286,7 +362,42 @@ function calculateAndDisplayRoute (directionsService,directionsRenderer,origin,h
         var modalKey = "direction-error";
         modalHandler(modalKey);
     });
-    
+    //package essential data and call save function to store to localStorage for recall
+    var storedTrip =[]
+    var tripId = originInput.results[0].address_components[0].short_name + '-' + hotel.name;
+    console.log(tripId)
+    storedTrip.push(tripId)
+    storedTrip.push(userInput);
+    storedTrip.push(hotel);
+    saveTrip(storedTrip)    
+}
+//creates buttons for previous searches; enables user to recall previous trips searched in the same session
+var recallHandler =function(userInput, hotelSelected){
+    var recallButton = document.createElement("button");
+    var recallString = userInput[1].results[0].address_components[0].short_name + '-'+ hotelSelected.name + ' Trip'
+    recallButton.textContent = recallString
+    recallButton.setAttribute("id", recallString)
+    recallContainerEl.append(recallButton)
+    console.log('recallHandler called')
+}
+//sends data package to localStorage for later recall
+var saveTrip = function(storedTrip) {
+    console.log('saveTrip called')
+    console.log(storedTrip[0])
+    localStorage.setItem(storedTrip[0]+' Trip',JSON.stringify(storedTrip))
+}
+//when recall button is clicked, takes unique data from localStorage, unpacks it and sends to initTravelMap
+var loadTrip = function(event){
+    event.preventDefault
+    var trip = event.target.getAttribute("id")
+    var loadedTripString = localStorage.getItem(trip);
+    var loadedTrip = JSON.parse(loadedTripString);
+    console.log(loadedTrip)
+    loadedTripData = loadedTrip.splice(1,3);
+    console.log(loadedTripData);
+    var loadedUserInput = loadedTripData[0];
+    var loadedHotelInput = loadedTripData[1];
+    initTravelMap(loadedUserInput,loadedHotelInput);
 }
 
 //--------------------------field resetter---------------------------:
@@ -300,6 +411,8 @@ var reset = function(){
     initMap();
     directionsPanel.replaceChildren();
 }
+
+
 
 //--------------------------modal handler----------------------------:
 //switch statement depends on modalKey; triggered by error or invalid input
@@ -365,8 +478,7 @@ var modalHandler = function (modalKey) {
     }
 }
 
-//----------------------addEventListeners-------------------------:
-
+//----------------------Global addEventListeners-------------------------:
+recallContainerEl.addEventListener("click",loadTrip)
 hotelContainerEl.addEventListener("click", selectedHotel)
 fetchButtonEl.addEventListener("click",inputValidation);
-
